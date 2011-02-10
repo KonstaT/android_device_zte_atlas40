95a515a6f460c97c0914f0dea1c2653444c48655
Shashank Mittal <mittals@codeaurora.org>
2011-02-09 23:41:55 -0800

    [msm7627a]: Inital commit for msm7627a target.
    Change-Id: I0c4c3ed9b289c542a0c373379e3c2ca93fe1dd61

---
+++ AndroidBoard.mk
@@ -0,0 +1,63 @@
+LOCAL_PATH := $(call my-dir)
+
+#----------------------------------------------------------------------
+# Compile (L)ittle (K)ernel bootloader and the nandwrite utility
+#----------------------------------------------------------------------
+ifneq ($(strip $(TARGET_NO_BOOTLOADER)),true)
+
+# Compile
+include bootable/bootloader/lk/AndroidBoot.mk
+
+INSTALLED_BOOTLOADER_TARGET := $(PRODUCT_OUT)/bootloader
+file := $(INSTALLED_BOOTLOADER_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_BOOTLOADER) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+# Copy nandwrite utility to target out directory
+INSTALLED_NANDWRITE_TARGET := $(PRODUCT_OUT)/nandwrite
+file := $(INSTALLED_NANDWRITE_TARGET)
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_NANDWRITE) | $(ACP)
+	$(transform-prebuilt-to-target)
+endif
+
+#----------------------------------------------------------------------
+# Compile Linux Kernel
+#----------------------------------------------------------------------
+ifeq ($(KERNEL_DEFCONFIG),)
+    KERNEL_DEFCONFIG := msm7627a_defconfig
+endif
+
+include kernel/AndroidKernel.mk
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := surf_keypad.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+file := $(TARGET_OUT_KEYLAYOUT)/surf_keypad.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/surf_keypad.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT_KEYLAYOUT)/7k_handset.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/7k_handset.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/etc/vold.fstab
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/vold.fstab | $(ACP)
+	$(transform-prebuilt-to-target)
+
+ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
+file := $(TARGET_OUT)/etc/wifi/wpa_supplicant.conf
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/wpa_supplicant.conf | $(ACP)
+	$(transform-prebuilt-to-target)
+endif
---
+++ msm7627a.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, device/qcom/common/common.mk)
+
+PRODUCT_NAME := msm7627a
+PRODUCT_DEVICE := msm7627a
