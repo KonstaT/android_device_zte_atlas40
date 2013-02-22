73f1674c3c343324ad77351a9875d760122fabc9
Konsta <konsta09@gmail.com>
2013-02-22 12:52:02 +0200

    this is not a multi-core device
    Change-Id: I125e8fb11bc4cd0ac5ed0c4c3021ff3b49461c07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 
-TARGET_CPU_SMP := true
 TARGET_CORTEX_CACHE_LINE_32 := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
