3bbae8dbf38dcba98961c5d339def0538607fec3
Konsta <konsta09@gmail.com>
2013-04-03 19:23:11 +0300

    switch to full_phone
    This will bring back live wallpapers, ringtones, etc that
    some might consider 'bloat'.
    
    Change-Id: I6af7fb46a433bac5cde624030440a1721cf23c9d

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/zte/atlas40/atlas40.mk)
