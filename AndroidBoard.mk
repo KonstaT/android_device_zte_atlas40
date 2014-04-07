b17304d9030c1414dee63e2a9fad9a8b5ab39b65
Konsta <konsta09@gmail.com>
2014-04-07 22:46:00 +0300

    changes for kernel build
    Also not building bootloader or radio.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,34 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 #----------------------------------------------------------------------
-# Compile (L)ittle (K)ernel bootloader and the nandwrite utility
-#----------------------------------------------------------------------
-ifneq ($(strip $(TARGET_NO_BOOTLOADER)),true)
-
-# Compile
-include bootable/bootloader/lk/AndroidBoot.mk
-
-INSTALLED_BOOTLOADER_TARGET := $(PRODUCT_OUT)/bootloader
-file := $(INSTALLED_BOOTLOADER_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_BOOTLOADER) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-# Copy nandwrite utility to target out directory
-INSTALLED_NANDWRITE_TARGET := $(PRODUCT_OUT)/nandwrite
-file := $(INSTALLED_NANDWRITE_TARGET)
-ALL_PREBUILT += $(file)
-$(file) : $(TARGET_NANDWRITE) | $(ACP)
-	$(transform-prebuilt-to-target)
-endif
-
-#----------------------------------------------------------------------
 # Compile Linux Kernel
 #----------------------------------------------------------------------
-ifeq ($(KERNEL_DEFCONFIG),)
-    KERNEL_DEFCONFIG := msm7627a-perf_defconfig
-endif
-
 include kernel/AndroidKernel.mk
 
 file := $(INSTALLED_KERNEL_TARGET)
@@ -139,13 +113,4 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_DATA)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
-endif
-#----------------------------------------------------------------------
-# Radio image
-#----------------------------------------------------------------------
-ifeq ($(ADD_RADIO_FILES), true)
-radio_dir := $(LOCAL_PATH)/radio
-RADIO_FILES := $(shell cd $(radio_dir) ; find . -iname '*.ENC')
-$(foreach f, $(RADIO_FILES), \
-    $(call add-radio-file,radio/$(f)))
 endif
