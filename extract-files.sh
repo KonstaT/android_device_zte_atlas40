8f5e1ebfa04554f62dc6ab83bd93036afc8c38e1
Konsta <konsta09@gmail.com>
2014-06-24 18:34:14 +0300

    use common proprietary extraction scripts
    Change-Id: I8c1e68d9cad64730c77a992ecff43520ab2d9ee1

--- extract-files.sh
+++ extract-files.sh
@@ -1,40 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+export VENDOR=zte
+export DEVICE=atlas40
 
-VENDOR=zte
-DEVICE=atlas40
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    if [ "$SRC" = "adb" ]; then
-      adb pull /system/$FILE $BASE/$FILE
-    else
-      cp $SRC/system/$FILE $BASE/$FILE
-    fi
-
-done
-
-./setup-makefiles.sh
+../../../vendor/cm/tools/extract-files.sh $@
