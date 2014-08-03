2bba386fbb2bde3b959fa34298547b69e2483555
Konsta <konsta09@gmail.com>
2014-08-03 14:53:56 +0300

    bluetooth: disable unsupported features
    Change-Id: I609d90a863342587873a203efadb6f1aa6adc7c5

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Blade III"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_DEF_LOCAL_NAME	"Blade III"
+#define BTA_DISABLE_DELAY	1000 /* in milliseconds */
+#define BLE_INCLUDED		FALSE
+#define BTA_GATT_INCLUDED	FALSE
+#define SMP_INCLUDED		FALSE
 
 #endif
